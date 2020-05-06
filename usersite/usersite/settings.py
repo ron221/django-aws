c13087383a02f76737637d165c554e0cb2b07914
Ron <ronyeh0221@gmail.com>
2020-05-06 20:54:58 +0800

    change the link of css

--- usersite/usersite/settings.py
+++ usersite/usersite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'usersite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'usersite/main/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
