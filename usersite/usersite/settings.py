6e7a59b8a302be0919e10d1c52db05b3b7705fd7
Ron <ronyeh0221@gmail.com>
2020-05-06 18:50:24 +0800

    modify static file path

--- usersite/usersite/settings.py
+++ usersite/usersite/settings.py
@@ -14,10 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# 'DIRS': [
-#         os.path.join(BASE_DIR, 'templates')
-#     ]
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -59,7 +55,7 @@ ROOT_URLCONF = 'usersite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
